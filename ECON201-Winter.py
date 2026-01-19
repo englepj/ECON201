3bdf8d1e663f8ef6dbd6792c0c17e2af399af32c
englepj <par.engle@gmail.com>
2026-01-19 08:29:06 -0800

    Update ECON201-Winter.py

--- ECON201-Winter.py
+++ ECON201-Winter.py
@@ -1055,7 +1055,7 @@ def page_cost_curve_explorer():
     # This yields increasing MP early, then diminishing MP, then near-zero.
     base_mp = 120.0
     gain = 100.0
-    decay = 10.5
+    decay = gain / 15
 
     employees = np.arange(1, max_employees + 1)
     mp = base_mp + gain * employees - decay * (employees ** 2)
@@ -1431,6 +1431,7 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
 
 
 
