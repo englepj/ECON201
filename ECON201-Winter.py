7c28623dbf56d46d567994d90cf3a4f8ce0c412b
englepj <par.engle@gmail.com>
2026-01-19 10:08:58 -0800

    Update ECON201-Winter.py

--- ECON201-Winter.py
+++ ECON201-Winter.py
@@ -1054,9 +1054,9 @@ def page_cost_curve_explorer():
 
     # MP(L) = A + B*L - D*L^2  (inverted-U if D>0)
     # Tune these three:
-    A = 120.0      # baseline MP
-    B = 18.0       # early gains from specialization
-    D = 0.5        # diminishing returns strength (bigger => earlier/faster drop)
+    A = 180.0      # baseline MP
+    B = 28.0       # early gains from specialization
+    D = 1.5        # diminishing returns strength (bigger => earlier/faster drop)
 
     mp = A + B*employees - D*(employees**2)
 
@@ -1447,6 +1447,7 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
 
 
 
