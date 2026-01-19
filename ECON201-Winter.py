57f3cb44aeac03c687b45f932c6a5d6d79b41ff8
englepj <par.engle@gmail.com>
2026-01-19 10:26:43 -0800

    Update ECON201-Winter.py

--- ECON201-Winter.py
+++ ECON201-Winter.py
@@ -1055,8 +1055,8 @@ def page_cost_curve_explorer():
     # MP(L) = A + B*L - D*L^2  (inverted-U if D>0)
     # Tune these three:
     A = 200.0
-    B = 30.0
-    D = 0.7
+    B = 40.0
+    D = 9
 
     mp = A + B*employees - D*(employees**2)
 
@@ -1448,6 +1448,7 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
 
 
 
